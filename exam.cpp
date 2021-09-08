fdf3cfaa282467feeed9cb597501fcab656276d7
dzuizz <59071340+dzuizz@users.noreply.github.com>
2021-09-08 14:08:52 +0800

    Solved exam

---
+++ exam.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    if(x==1) cout << "Hello World";
+    else{
+        long long a, b;
+        cin >> a >> b;
+        cout << a*b << endl;
+    }
+}
