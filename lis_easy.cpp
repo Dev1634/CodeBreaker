7a943ccc99f2d5e35c822cc5c5dabe7625de7cc6
dzuizz <59071340+dzuizz@users.noreply.github.com>
2022-06-20 18:23:11 +0800

    Solved lis_easy ( with DP - O(N^2) )

--- lis_easy.cpp
+++ lis_easy.cpp
@@ -1,33 +1,23 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
-typedef long long ll;
-
-ll n;
-vector<ll> v, dist;
-
-void f(ll cur) {
-    if (dist[cur] != -1) return;
-
-    ll res = 0;
-    for (ll i = cur + 1; i < n; i++) {
-        f(i);
-        if (v[i] > v[cur]) res = max(res, dist[i]);
-    }
-    dist[cur] = res + 1;
-}
 
 int main() {
-    cin >> n;
-    v = dist = vector<ll>(n);
-    fill(dist.begin(), dist.end(), -1);
+    int n; cin >> n;
+    vector<int> v(n), dist(n, 1);
 
-    for (ll i = 0; i < n; i++) cin >> v[i];
+    for (int i = 0; i < n; i++) cin >> v[i];
 
-    for (ll i = 0; i < n; i++) f(i);
-    
-    ll ans = 0;
-    for (ll i = 0; i < n; i++) {
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (v[j] < v[i]) {
+                dist[i] = max(dist[i], dist[j] + 1);
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
         ans = max(ans, dist[i]);
     }
 
