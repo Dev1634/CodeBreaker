225a6ec3569d5dd9c9b5aef2e86f9e7ab9fa9587
dzuizz <59071340+dzuizz@users.noreply.github.com>
2022-06-20 11:33:32 +0800

    Solved 3nplus1

---
+++ 3nplus1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    int n; cin >> n;
+    while(n > 1) {
+        cout << n << " ";
+        if(n % 2 == 0) n /= 2;
+        else n = 3 * n + 1;
+    }
+    cout << n << endl;
+}
