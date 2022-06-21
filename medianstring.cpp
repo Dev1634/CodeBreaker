77ae356622eb360f85ef20ccb51420c8c2333542
dzuizz <59071340+dzuizz@users.noreply.github.com>
2022-06-21 21:26:10 +0800

    Solved medianstring

---
+++ medianstring.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int n; cin >> n;
+    int ans;
+    for (int i = 0; i < (n+1)/2; i++) cin >> ans;
+    cout << ans << endl;
+}
