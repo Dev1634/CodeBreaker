1cbba3ba01c1ac69ca6c87467049a2802ba038ab
dzuizz <59071340+dzuizz@users.noreply.github.com>
2022-06-23 07:36:48 +0800

    Solved greeting

---
+++ greeting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    string s; getline(cin, s);
+    cout << s << " ni hao, xin nian kuai le!";
+}
