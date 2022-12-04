41003078732d27161115fcc44a9f4fa5264188bf
dzuizz <59071340+dzuizz@users.noreply.github.com>
2022-12-04 08:26:08 +0700

    Solved sandd Used Binary Search

---
+++ sandd/sandd.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+#include <cmath>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+#include "sandd.h"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef map<ll, ll> mll;
+typedef set<ll> sll;
+typedef queue<ll> qll;
+typedef queue<pll> qpll;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (ll) x.size()
+#define fr front()
+#define bk back()
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define FOR(i, a, b, inc) for (ll i = a; i < b; i += inc)
+#define RFOR(i, a, b, inc) for (ll i = b - 1; i >= a; i -= inc)
+#define REP(i, a) FOR(i, 0, a, 1)
+#define RREP(i, a) RFOR(i, 0, a, 1)
+#define each(a, x) for (auto& a: x)
+
+const ll INF = 1e9;
+const ll M = 1e9 + 7;
+
+int getPrice (int P) {
+    ll l = 0, r = P;
+
+    while (l < r) {
+        ll m = (l + r) / 2;
+        
+        if (supply(m) < demand(m)) l = m + 1;
+        else r = m;
+    }
+
+    return l;
+}
