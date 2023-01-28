da122c37deedd5e56e72583cc614291281fbbd59
dzuizz <59071340+dzuizz@users.noreply.github.com>
2023-01-29 00:13:04 +0800

    Solved aliens

---
+++ aliens.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+#include <cmath>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef map<ll, ll> mll;
+typedef set<ll> sll;
+typedef queue<ll> qll;
+typedef queue<pll> qpll;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (ll) x.size()
+#define fr front()
+#define bk back()
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define FOR(i, a, b, inc) for (ll i = a; i < b; i += inc)
+#define RFOR(i, a, b, inc) for (ll i = b - 1; i >= a; i -= inc)
+#define REP(i, a) FOR(i, 0, a, 1)
+#define RREP(i, a) RFOR(i, 0, a, 1)
+#define each(a, x) for (auto& a: x)
+
+const ll INF = 1e9;
+const ll M = 1e9 + 7;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    ll n; cin >> n;
+
+    map<string, ll> ans;
+    map<string, pll> m;
+
+    REP(i, n) {
+        string s; cin >> s;
+        ll x; cin >> x;
+
+        if (x == 1) {
+            ll y; cin >> y;
+
+            if (m[s].se == y) {
+                m[s] = {++m[s].fi, y};
+            } else {
+                m[s] = {1, y};
+            }
+            
+            ans[s] = max(ans[s], m[s].fi);
+        } else {
+            cout << ans[s] << '\n';
+        }
+    }
+}
