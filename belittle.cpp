319980b84c854bb38f688df8d127bfe51dd37f97
dzuizz <59071340+dzuizz@users.noreply.github.com>
2023-01-28 14:50:56 +0800

    Solved belittle

---
+++ belittle.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include <climits>
+#include <cstdio>
+#include <cmath>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <map>
+#include <set>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+typedef vector<pll> vpll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef map<ll, ll> mll;
+typedef set<ll> sll;
+typedef queue<ll> qll;
+typedef queue<pll> qpll;
+
+#define all(x) x.begin(), x.end()
+#define sz(x) (ll) x.size()
+#define fr front()
+#define bk back()
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define FOR(i, a, b, inc) for (ll i = a; i < b; i += inc)
+#define RFOR(i, a, b, inc) for (ll i = b - 1; i >= a; i -= inc)
+#define REP(i, a) FOR(i, 0, a, 1)
+#define RREP(i, a) RFOR(i, 0, a, 1)
+#define each(a, x) for (auto& a: x)
+
+const ll INF = 1e9;
+const ll M = 1e9 + 7;
+
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a%b);
+}
+ll pow(ll a, ll b) {
+    if (b == 0) return 1;
+    if (b == 1) return a;
+    ll x = pow(a, b/2);
+    if (b%2 == 0) return x*x;
+    return x*x*a;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    ll n, q; cin >> n >> q;
+    ll a[n], pref[n], suff[n];
+
+    REP(i, n) cin >> a[i];
+
+    ll mn = a[0], mx = a[0];
+    REP(i, n) {
+        mn = min(mn, a[i]);
+        mx = max(mx, a[i]);
+
+        pref[i] = mn * mx;
+    }
+
+    mn = a[n - 1], mx = a[n - 1];
+    RREP(i, n) {
+        mn = min(mn, a[i]);
+        mx = max(mx, a[i]);
+
+        suff[i] = mn * mx;
+    }
+
+    while (q--) {
+        ll s, l, r; cin >> s >> l >> r;
+
+        cout << min(pref[s - 1] + l, suff[s + 1] + r) << '\n';
+    }
+}
